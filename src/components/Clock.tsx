6d6269e24cec20a2980c3bc1887bb1da4fddaeb2
ignacy <ignacybanet@cyfroweszkoly.pl>
2025-05-29 10:58:11 +0200

    create basic page layout and necessary elements along with css

---
+++ src/components/Clock.tsx
@@ -0,0 +1,13 @@
+import '../css/Clock.css'
+
+const Clock = () => {
+    return (
+        <>
+            <div className="clock">
+                <h2>Clock</h2>
+            </div>
+        </>
+    )
+}
+
+export default Clock
