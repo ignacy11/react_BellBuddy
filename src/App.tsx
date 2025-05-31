4800bc9203cd89bca85f4593ac3133dab917267b
ignacy11 <ignacy.banet@cyfroweszkoly.pl>
2025-05-31 23:48:51 +0200

    add form functionality with logging data to console (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import './css/App.css'
 import './css/Clock.css'
 import Clock from "./components/Clock.tsx";
+import AddStudentForm from "./components/AddStudentForm";
+// @ts-ignore
 import StudentList from './components/StudentList.tsx';
+import {useState} from "react";
 
 function App() {
 
-  return (
+    const [activeTab, setActiveTab] = useState("StudentList");
+
+    return (
     <>
       <header>
-          <button className="add-student">Dodaj ucznia do listy</button>
+          <button className="add-student header-button" onClick={() => setActiveTab("AddStudentForm")}>Dodaj ucznia do listy</button>
+          <button className="open-student-list header-button" onClick={() => setActiveTab("StudentList")}>Otwórz listę uczniów</button>
           <h1>Bell Buddy</h1>
           <Clock/>
       </header>
         <main>
-            <StudentList/>
-            {/*TODO: dynamically change main content*/}
+            {activeTab === "StudentList" && <StudentList/>}
+            {activeTab === "AddStudentForm" && <AddStudentForm/>}
         </main>
     </>
   )
