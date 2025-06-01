40ee708730d6e84cca9241742f69ddaae33d66ed
ignacy11 <ignacy.banet@cyfroweszkoly.pl>
2025-06-01 18:21:41 +0200

    Form functionality and clock (#3)
    * add form functionality with logging data to console
    
    * add student list management through form, add clock, add type definitions in Student.ts

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,10 +1,23 @@
 import '../css/Clock.css'
+import {useEffect, useState} from "react";
 
 const Clock = () => {
+    const now = new Date()
+    const [currentTime, setCurrentTime] = useState(now.toLocaleTimeString("pl"))
+
+    useEffect(() => {
+            const timer = setInterval( () => {
+                setCurrentTime(new Date().toLocaleTimeString("pl"))
+            }, 1000)
+            return () => clearInterval(timer)
+        },
+        []
+    )
+
     return (
         <>
             <div className="clock">
-                <h2>Clock</h2>
+                <h2>{currentTime.toString()}</h2>
             </div>
         </>
     )
--- src/components/StudentList.tsx
+++ src/components/StudentList.tsx
@@ -1,24 +1,29 @@
 import '../css/StudentList.css'
 import StudentItem from './StudentItem'
+import type {Student} from '../Student.ts'
 
-const StudentList = () => {
+const StudentList = ({ studentItemsList }: { studentItemsList: Student[] }) => {
     return (
         <>
             <table>
                 <thead>
-                    <tr>
-                        <th className={"name"}>Imię</th>
-                        <th className={"last-name"}>Nazwisko</th>
-                        <th className={"attendance"}>Obecność</th>
-                    </tr>
+                <tr>
+                    <th className="name">Imię</th>
+                    <th className="last-name">Nazwisko</th>
+                    <th className="attendance">Obecność</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {/*TODO: StudentListItem components*/}
-                <StudentItem/>
+                {
+                    studentItemsList.map((item) => (
+                            <StudentItem {...item} />
+                        )
+                    )
+                }
                 </tbody>
             </table>
         </>
     )
 }
 
-export default StudentList
+export default StudentList;
